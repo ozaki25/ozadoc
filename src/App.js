c31897e1b685cfc314c83c758e2cbeb84d9ebf4f
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-27 18:43:40 +0900

    add file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Top from "./components/Top";
+
+function App() {
+  return (
+    <div>
+      <Top />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePosts = `subscription OnCreatePosts {
+  onCreatePosts {
+    id
+    name
+    url
+    description
+  }
+}
+`;
+export const onUpdatePosts = `subscription OnUpdatePosts {
+  onUpdatePosts {
+    id
+    name
+    url
+    description
+  }
+}
+`;
+export const onDeletePosts = `subscription OnDeletePosts {
+  onDeletePosts {
+    id
+    name
+    url
+    description
+  }
+}
+`;
